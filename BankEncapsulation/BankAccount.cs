799a65166fa1586479f52c4d2cff6e653e1fd5a7
Brian H <humphreybrian58@gmail.com>
2020-09-02 12:25:31 -0400

    Add project files.

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount() {}
+        private double balance = 0;
+
+        public double Deposit(double value) {
+            return balance += value;
+        }
+        
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAcct = new BankAccount();
+
+            Console.WriteLine(myAcct.Deposit(200));
+
+            Console.WriteLine(myAcct.GetBalance());
+        }
+    }
+}
